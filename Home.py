14574c148773a3b1f930b956204d69d2857c37ad
Priya k <priyamadurai2022@gmail.com>
2023-06-29 07:59:54 -0700

    Home page is now complete

--- Home.py
+++ Home.py
@@ -1,11 +1,8 @@
-# pandas help read csv content
 import streamlit as st
-import pandas
 from streamlit_timeline import timeline
 
 # Set the page configuration
-st.set_page_config(page_title="devi karuppiah's portfolio", layout="wide", page_icon='👧🏽✨')
-
+st.set_page_config(page_title="devikaruppiah's portfolio", layout="wide", page_icon='👧🏽✨')
 
 col1, col2 = st.columns(2)
 
@@ -16,23 +13,17 @@ with col2:
     st.title("Devi Karuppiah")
     content = """
     Hi, I am Devi! ✨
-    
+
     Passionate about effective communication from a young age, on my completing my undergrad, I joined a multinational IT firm's program management group, improving technical documents across the SDLC, as a technical writer. Seeking growth, I pursued a Master's in IT Management in the US. With experience in market research at Research Now, project portfolio management at Deloitte, and cross-functional leadership at Microsoft, I thrive in diverse domains.
 
-    Both at life and work, I always strive to see the big picture and embrace challenges as opportunities. As a strategic problem-solver, I possess core skill of project management, emotional intelligence and being comfortable with conflict. I bring enthusiasm, purpose, and a 'roll-up-your-sleeves' attitude to every task and project I've delivered . Being purpose driven, I'm always on the lookout to make a positive difference in people's lives.
-
-
-
-
-
+    Both at life and work, I always strive to see the big picture and embrace challenges as opportunities. As a strategic problem-solver, I possess core skill of project management, emotional intelligence and being comfortable with conflict. I bring enthusiasm, purpose, and a 'roll-up-your-sleeves' attitude to every task and project I've delivered. Being purpose driven, I'm always on the lookout to make a positive difference in people's lives.
     """
     st.info(content)
 
 content2 = """
-Scroll down to read more about me. Feel free to contact me at https://www.linkedin.com/in/devipriyakaruppiah/!
+Scrolldown to read more about me. Feel free to contact me at [LinkedIn](https://www.linkedin.com/in/devipriyakaruppiah/)!
 """
-st.write(content2)
-
+st.markdown(content2)
 
 st.title('Career Snapshot')
 
@@ -41,33 +32,36 @@ with st.spinner(text="Building line"):
         data = f.read()
         timeline(data, height=500)
 
+# Display heading
+st.title('Education📚')
+
+# Define table data
+table_data = [
+    ['Qualification', 'Major', 'Graduation Year', 'Institute', 'Score'],
+    ["Bachelor's Degree", 'Btech', '2012', 'SASTRA University', '8.1 (Honors)'],
+    ["Master's Degree", 'Information Technology Management', '2016', 'UT Dallas', '3.6'],
+    ['Certification', 'AI Product Management Specialization', '2023', 'Duke University via Coursera', 'NA'],
+    ['Certification', 'Mastering OpenAI Python APIs', '2023', 'Udemy', 'NA']
+]
+
+# Display the table
+st.table(table_data)
 
 # Display heading
-    st.title('Education 📚')
-def main():
-    # Define table data
-    table_data = [
-        ['Qualification', 'Major', 'Graduation Year', 'Institute', 'Score'],
-        ['Bachelor\'s Degree', 'Btech', '2012', 'SASTRA University', '8.1 (Honors)'],
-        ['Master\'s Degree', 'Information Technology Management', '2016', 'UT Dallas', '3.6'],
-        ['Certification', 'AI Product Management Specialization', '2023', 'Duke University via Coursera', 'NA'],
-        ['Certification', 'Mastering OpenAI Python APIs', '2023', 'Udemy', 'NA']
-    ]
+st.title('Achievements🥇')
 
-    # Display the table
-    st.table(table_data)
+# Define achievements bullet points
+achievements = [
+    "Cohort’19, VolunteerNow Young Leader Council: YLC trains young professionals to serve on non-profit boards",
+    "Completed Harvard’s Sustainable Business Strategy, a program that develops purpose-driven leaders, Dec 2019",
+    "Served as a board member on Deloitte's Step-up DFW chapter - an organization that offered pro bono consulting to NGOs",
+    "Commencement Speaker addressing the class of Fall 2016 at The University of Texas at Dallas",
+    "Dean’s Advisory Council, UT Dallas; Leader of school-wide social media project impacting 8200+ students",
+    "Recipient of Dean’s Impact Scholarship for displaying extraordinary impact to Jindal school of Management",
+    "Awarded JSOM Perspectives Top Grad Contributor 2016 for contributing the most to the school’s official blog",
+    "Editor-in-chief of BioSpace Newsletter, SASTRA University; led a team of 4 to launch a successful department-wide monthly newsletter to an academic community of 500+ members",
+    "Recipient of two awards ('On the Spot', 'CLP Faculty') from Learning and Development department at Tata Consultancy Services, for outstanding contribution to the presentation sessions conducted for Associates"
+]
 
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
+# Render the achievements as bullet points using st.markdown()
+st.markdown("<ul>" + "".join(f"<li>{achievement}</li>" for achievement in achievements) + "</ul>", unsafe_allow_html=True)
---
+++ pages/Layoff or Sabbatical.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.header("Layoff or Sabbatical")
